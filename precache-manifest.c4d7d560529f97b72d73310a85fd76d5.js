b2bcdd1245db9e59f9f17d555d80afea603cec5c
Hero07 <jessemannu@gmail.com>
2019-12-02 10:53:07 -0600

    Updates

---
+++ precache-manifest.c4d7d560529f97b72d73310a85fd76d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f176d8f4e4edaa59fe9602a6b1799cdd",
+    "url": "/ModelPortafolio/index.html"
+  },
+  {
+    "revision": "296f74176f672b880499",
+    "url": "/ModelPortafolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ede6ddd5a2c82625fa05",
+    "url": "/ModelPortafolio/static/js/2.ddc0f638.chunk.js"
+  },
+  {
+    "revision": "296f74176f672b880499",
+    "url": "/ModelPortafolio/static/js/main.b47e4c63.chunk.js"
+  },
+  {
+    "revision": "3b52050c3dca5bf05fec",
+    "url": "/ModelPortafolio/static/js/runtime-main.875c18b0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ModelPortafolio/static/media/logo.25bf045c.svg"
+  }
+]);
